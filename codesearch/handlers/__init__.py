8373ebb19e9eefce2f08c74ec706a93b773c4915
Dmitry Guzeev <dmitri.guzeev@gmail.com>
2021-09-16 00:56:58 +0300

    Lazy-load handlers

--- codesearch/handlers/__init__.py
+++ codesearch/handlers/__init__.py
@@ -1,20 +1,39 @@
 import os
-from codesearch.handlers.cpp import CppHandler
-from codesearch.handlers.python import PythonHandler
-from codesearch.handlers.js import JSHandler
+
+
+def import_python_handler():
+    from codesearch.handlers.python import PythonHandler
+
+    return PythonHandler
+
+
+def import_cpp_handler():
+    from codesearch.handlers.cpp import CppHandler
+
+    return CppHandler
+
+
+def import_js_handler():
+    from codesearch.handlers.js import JSHandler
+
+    return JSHandler
 
 
 handlers = {
-    ".py": PythonHandler,
-    ".cpp": CppHandler,
-    ".hpp": CppHandler,
-    ".cc": CppHandler,
-    ".h": CppHandler,
-    ".cxx": CppHandler,
-    ".js": JSHandler,
+    ".py": import_python_handler,
+    ".cpp": import_cpp_handler,
+    ".hpp": import_cpp_handler,
+    ".cc": import_cpp_handler,
+    ".h": import_cpp_handler,
+    ".cxx": import_cpp_handler,
+    ".js": import_js_handler,
 }
+
 
 def handler_for_file_type(f: str):
     _, ext = os.path.splitext(f)
-    handler = handlers.get(ext, None)
+    handler_importer = handlers.get(ext, None)
+    if handler_importer is None:
+        return None
+    handler = handler_importer()
     return handler
